54d02a246c9a99f309a63b08c13c8a2e76aaccaf
mijishin <ygmjsy@naver.com>
2024-08-09 15:31:21 +0900

    todo list

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+	<header>
+		<h1 class="">TODO it!</h1>
+	</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1{
+	margin:2.5rem 0 1.5rem;
+	font-weight: 900;
+	color: #333;
+}
+</style>
